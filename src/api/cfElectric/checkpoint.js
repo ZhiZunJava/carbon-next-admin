67785d1172ec85ee5fff76dfaa5cd0b269bee247
程安宁 <3308869544@qq.com>
2025-04-20 16:10:17 +0800

    feat(api, view): 能效预警相关功能

--- src/api/cfElectric/checkpoint.js
+++ src/api/cfElectric/checkpoint.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-// 查询能效检测点列表
+// 查询能效预警列表
 export function listCheckpoint(query) {
   return request({
     url: '/cfElectric/checkpoint/list',
@@ -9,7 +9,7 @@ export function listCheckpoint(query) {
   })
 }
 
-// 查询能效检测点详细
+// 查询能效预警详细
 export function getCheckpoint(id) {
   return request({
     url: '/cfElectric/checkpoint/' + id,
@@ -17,7 +17,7 @@ export function getCheckpoint(id) {
   })
 }
 
-// 新增能效检测点
+// 新增能效预警
 export function addCheckpoint(data) {
   return request({
     url: '/cfElectric/checkpoint',
@@ -26,7 +26,7 @@ export function addCheckpoint(data) {
   })
 }
 
-// 修改能效检测点
+// 修改能效预警
 export function updateCheckpoint(data) {
   return request({
     url: '/cfElectric/checkpoint',
@@ -35,19 +35,10 @@ export function updateCheckpoint(data) {
   })
 }
 
-// 删除能效检测点
+// 删除能效预警
 export function delCheckpoint(id) {
   return request({
     url: '/cfElectric/checkpoint/' + id,
     method: 'delete'
-  })
-}
-
-// 导出能效检测点
-export function exportCheckpoint(query) {
-  return request({
-    url: '/cfElectric/checkpoint/export',
-    method: 'get',
-    params: query
   })
 }
